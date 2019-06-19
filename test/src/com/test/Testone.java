7f7356d0f2f1b8fe208332c46c14c699092ed793
ishucec <51887648+ishucec@users.noreply.github.com>
2019-06-18 22:32:14 -0500

    sample progrms (#1)

---
+++ test/src/com/test/Testone.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class Testone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		
+		a=a^b;
+		b=a^b;
+		a=a^b;
+		String s=new String("ishwar");
+		System.out.println(a+"value"+b);
+		
+
+	}
+
+}
